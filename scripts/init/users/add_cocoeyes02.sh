c03603fcf9c93f994c577093dea8c70082daa6c3
IkezoeMakoto <150283t@gmail.com>
2019-08-11 16:01:29 +0900

    chshを共通に

--- scripts/init/users/add_cocoeyes02.sh
+++ scripts/init/users/add_cocoeyes02.sh
@@ -9,5 +9,3 @@ su -l cocoeyes02 -c '
 mkdir -m 0700 .ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCyW/beTNQC1ppmtiME2M7IFjJRNuMgRbRbJ5Ka2kK8scyp2KEgThJOfvWdGFMBMSY3dnlL2XBCRDpEaBo/qCRlMA2KZiBUVfCYCrmUnjUZ0uyIWHAHST8rspjmRlplanPLkShfDRgO6iE5mW2Aqb2Pj20kEUR7ktE1Qz4j4REu59BD0GA7OBHGTrmvg2jGTBWOIXLwainjszU1iJQdNUFKBOulklaK894GBo+YeuNKRTFU81OPRXd+BIOZ8t/zvLR2jW5VqWnP9WE0CwE2Mza5kNxFH3kpp7mIxtTmPZkuDO/EJ5FKeAiotYo5rrDeQbiwwB9kDJU9eX83yL4DsZZp" > .ssh/authorized_keys
 chmod 700 .ssh/authorized_keys'
-## シェルを変更
-chsh -s /bin/bash cocoeyes02
--- scripts/init/users/add_seiichiakiba.sh
+++ scripts/init/users/add_seiichiakiba.sh
@@ -9,5 +9,3 @@ su -l seiichiakiba -c '
 mkdir -m 0700 .ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8E50PaUtBHVPuZu75tl/mqvs1FPnaKIVhxOyV3IVTZVXFZV4Yh9naO9wmuTtxREk65sOthOfaNeI9yVl9x6D7uAiZd/U9Qv9r4AoeQFLxMCmd0LN//EhOo4hHu+iN+v6f0tsg+X6iXVkeYM3EjPe9t8WmSDlhoZCjFx0+Rxl1/dLDJULrj/PPfBH0fOtKo3LyUNP+dT1tKyRSebi67ekRV5a8LqH5eOh0qgKm2naKb1q33BexB3NnQJlR69kBgyjDlj8JX9Mj9CJ5sfwUlEswVyTkXhuXtWHkj8Ax8EF0kM1wCEtFllz8U9yD7PAVgQ0h6SRlRaFqRaoWu4b+x1dp seiichiakiba@SeiichinoMacBook-puro.local" > .ssh/authorized_keys
 chmod 700 .ssh/authorized_keys'
-## シェルを変更
-chsh -s /bin/bash seiichiakiba
