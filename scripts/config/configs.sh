874750eb99b167611e62c3ba9eba510e6a3a0875
IkezoeMakoto <150283t@gmail.com>
2019-08-11 18:09:15 +0900

    backup script fix

--- scripts/config/configs.sh
+++ scripts/config/configs.sh
@@ -2,3 +2,5 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 . $SCRIPT_DIR/backup.sh /etc/nginx/nginx.conf
+. $SCRIPT_DIR/backup.sh /etc/my.cnf
+. $SCRIPT_DIR/backup.sh /etc/systemd/system/isucon.go.service
