e475f4a73e44aacffc342b8a42c9b05eb7c5c7ef
IkezoeMakoto <150283t@gmail.com>
2019-08-11 16:46:06 +0900

    バックアップ先を修正

--- scripts/config/backup.sh
+++ scripts/config/backup.sh
@@ -4,19 +4,19 @@
 # 使い方) ./conf_backup.sh /etc/nginx/nginx.conf
 
 # 設定ファイルの置き場所（scriptsの上に実際のパスごとコピーする /etc/nginx/nginx.conf -> ./etc/nginx/nginx.conf）
-PARENT_DIR=$(cd $(dirname $0);cd ../..;pwd)
+BACKUP_DIR=$(cd $(dirname $0);cd ../..;pwd)/backup
 ## デバッグ用出力
 #echo "#######"
 #echo "#debug#"
 #echo "#######"
 #echo "\$1 =" $1
 #echo "\$(basename \$1) =" $(basename $1)
-#echo "\$PARENT_DIR =" $PARENT_DIR
+#echo "\$BACKUP_DIR =" $BACKUP_DIR
 #echo "#########"
 #echo "#dry-run#"
 #echo "#########"
-#echo "rsync -R $1 $PARENT_DIR"
-#echo "ln -snf $PARENT_DIR$1 $1"
+#echo "rsync -R $1 $BACKUP_DIR"
+#echo "ln -snf $BACKUP_DIR$1 $1"
 
-rsync -R $1 $PARENT_DIR
-ln -snf $PARENT_DIR$1 $1
+rsync -R $1 $BACKUP_DIR
+ln -snf $BACKUP_DIR$1 $1
