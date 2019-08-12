c44db80f1abbdff353b20021742a79782465aaf4
IkezoeMakoto <150283t@gmail.com>
2019-08-12 16:30:51 +0900

    deploy script fix

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -eu
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ## 並列でデプロイする
 
 # server1
-cat deploy/app.sh | ssh isucon-server1 &
+cat ${SCRIPT_DIR}/deploy/app.sh | ssh isucon-server1 &
 # server2
-cat deploy/web.sh | ssh isucon-server2 &
+cat ${SCRIPT_DIR}/deploy/web.sh | ssh isucon-server2 &
 # server3
-cat deploy/database.sh | ssh isucon-server3 &
+cat ${SCRIPT_DIR}/deploy/database.sh | ssh isucon-server3 &
 
 wait
